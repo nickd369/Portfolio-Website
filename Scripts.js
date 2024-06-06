65319faa55050c0d1bac239ad40150fe624317c3
Nicholas <nickd369@gmail.com>
2024-06-05 20:19:15 -0400

    Added pictures and lightbox to about me

--- Scripts.js
+++ Scripts.js
@@ -50,3 +50,42 @@ if (!localStorage.getItem('mode')) {
     applyMode(storedMode);
 }
 
+var lightbox = document.getElementById('lightbox');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var lightboxImg = document.getElementById("lightbox-img");
+var thumbnails = document.querySelectorAll('.thumbnail');
+var currentIndex = 0;
+
+thumbnails.forEach((thumbnail, index) => {
+    thumbnail.onclick = function() {
+        lightbox.style.display = "block";
+        lightboxImg.src = this.src;
+        currentIndex = index;
+    }
+});
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    lightbox.style.display = "none";
+}
+
+function changeSlide(n) {
+    currentIndex += n;
+    if (currentIndex < 0) {
+        currentIndex = thumbnails.length - 1;
+    } else if (currentIndex >= thumbnails.length) {
+        currentIndex = 0;
+    }
+    lightboxImg.src = thumbnails[currentIndex].src;
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == lightbox) {
+        lightbox.style.display = "none";
+    }
+}
